5d002eebbced6004af60f7ccd04239e6c41ba672
lab04 <lab04@lab4-18>
2024-04-01 22:51:38 -0300

    Nova classe, etc.

--- ProjetoPOO2/src/edu/etec/poo2/code/ClassePrincipal.java
+++ ProjetoPOO2/src/edu/etec/poo2/code/ClassePrincipal.java
@@ -5,6 +5,26 @@ public class ClassePrincipal {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello, world!");
+		
+		ClasseConta objeto_c1 = new ClasseConta();
+		
+		objeto_c1.atributoNumeroConta = 123;
+		objeto_c1.atributoSaldoConta = 20_000;
+		objeto_c1.atributoTipoConta = "Salário";
+		
+		System.out.println(objeto_c1.atributoNumeroConta);
+		System.out.println(objeto_c1.atributoSaldoConta);
+		System.out.println(objeto_c1.atributoTipoConta);
+	
+		objeto_c1.cli = new ClasseCliente();
+		
+		objeto_c1.cli.clienteNomeConta = "José";
+		objeto_c1.cli.clienteCPFConta = 222;
+		objeto_c1.cli.clienteRenda = 4_000;
+		
+		System.out.println(objeto_c1.cli.clienteNomeConta);
+		System.out.println(objeto_c1.cli.clienteCPFConta);
+		System.out.println(objeto_c1.cli.clienteRenda);
 
 	}
 
